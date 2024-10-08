28bf939393748f4a5706f0d3393f3620a606451e
Fernando Carmine <carmine.fernando@gmail.com>
2024-10-08 20:51:22 -0300

    cambios con los repos

--- src/main/java/ar/com/educacionit/clase3/repository/ClienteRepository.java
+++ src/main/java/ar/com/educacionit/clase3/repository/ClienteRepository.java
@@ -1,9 +1,27 @@
 package ar.com.educacionit.clase3.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ar.com.educacionit.clase3.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNombre(String nombre);
+	List<Cliente> findByApellido(String apellido);
+	List<Cliente> findByNombreAndApellido(String nombre, String apellido);
+	List<Cliente> findByNombreOrApellido(String nombre, String apellido);
+	List<Cliente> findByEmailIsNotNull();
+	List<Cliente> findByEmailIsNull();
+	List<Cliente> findByApellidoOrderByNombreDesc(String apellido);
+	List<Cliente> findByNombreStartingWith(String nombre);
+	List<Cliente> findDistinctByProductosNombre(String nombre);
+	
+	@Query(value = "SELECT * FROM cliente WHERE apellido = :apellido", nativeQuery = true)
+	List<Cliente> buscarPorApellido(@Param("apellido") String apellido);
+	
 }
--- src/main/java/ar/com/educacionit/clase3/repository/ProductoRepository.java
+++ src/main/java/ar/com/educacionit/clase3/repository/ProductoRepository.java
@@ -5,6 +5,8 @@ package ar.com.educacionit.clase3.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ar.com.educacionit.clase3.model.Producto;
@@ -14,6 +16,13 @@ public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 	
 	
 	List<Producto> findByStockLessThan(Integer stock);
+	
+	@Query (value = "SELECT * FROM productos WHERE precio > :precio", nativeQuery = true)
+	List<Producto> buscarProductosPorPrecioMayor(@Param("precio") Double precio);
+	
+	@Query (value = "SELECT * FROM productos WHERE stock = 0", nativeQuery = true)
+	List<Producto> buscarProductosSinStock();
+	
 	
 	
 }
